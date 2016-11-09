ed84a7dddef81049b93d436a15d0f470bba0322f
Mousa Najafi <mousanajafi@users.noreply.github.com>
2016-11-09 11:01:02 +0330

    Update admin.js
    fix problem on "save" on save and back to product list

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-	$('.panel-footer').before('<input name="submitAndStay" type="hidden">');
-		$('#product_form .panel-footer').before('<input name="submitAddproductAndStay" type="hidden">');
+    $('#product_form').prepend('<input id="submitSaveProductForm" name="submitProductForm" type="hidden">');
+    
+    $(document).on('mouseenter', '#product_form button[name="submitAddproduct"]', function(){
+        $('#product_form input#submitSaveProductForm').attr('name','submitAddproduct');
+    });
+    
+    $(document).on('mouseenter', '#product_form button[name="submitAddproductAndStay"]', function(){
+        $('#product_form input#submitSaveProductForm').attr("name","submitAddproductAndStay");
+    });
 });
